c9bf804d4c5f35c141076f44b84a6e5c3ea6105e
Allahu99 <instaplayer884@gmail.com>
2026-01-04 23:27:31 -0300

    final: paginas servicos e precos no ar

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+# Pasta de arquivos estáticos (CSS, JS, imagens)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Templates HTML
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+# Rotas
+@app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/servicos", response_class=HTMLResponse)
+def servicos(request: Request):
+    return templates.TemplateResponse("servicos.html", {"request": request})
+
+@app.get("/precos", response_class=HTMLResponse)
+def precos(request: Request):
+    return templates.TemplateResponse("precos.html", {"request": request})
+
